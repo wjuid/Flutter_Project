09791903fe2d5a6043f6fd8b6517b6ff1a1114c9
shuige <wjuid@163.com>
2024-04-03 11:23:47 +0800

    4/3/2024  11:24 AM

---
+++ list_demo/README.md
@@ -0,0 +1,3 @@
+# list_demo
+
+A new Flutter project.
---
+++ list_demo/lib/main.dart
@@ -0,0 +1,54 @@
+import 'package:flutter/material.dart';
+
+enum ListDemoType {
+  oneline,
+  twoline,
+}
+
+class ListDemo extends StatelessWidget {
+  const ListDemo({super.key, required this.type});
+
+  final ListDemoType type;
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      theme: ThemeData(
+        colorScheme: ColorScheme.fromSeed(seedColor: Colors.blue),
+      ),
+      home: Scaffold(
+        appBar: AppBar(
+          automaticallyImplyLeading: false,
+          title: const Text('List demo'),
+        ),
+        body: ListView(
+          restorationId: 'list demo list view',
+          padding: const EdgeInsets.symmetric(vertical: 8),
+          children: [
+            for (var index = 1; index < 21; index++)
+              ListTile(
+                leading: ExcludeSemantics(
+                    child: CircleAvatar(
+                  backgroundImage: const AssetImage('assets/images/pic.png'),
+                  foregroundColor: Colors.white,
+                  radius: 25,
+                  child: Text(
+                    '$index',
+                    style: const TextStyle(fontSize: 20),
+                  ),
+                )),
+                title: Text('Item $index'),
+                subtitle: type == ListDemoType.twoline
+                    ? const Text('Secondary text')
+                    : null,
+              )
+          ],
+        ),
+      ),
+    );
+  }
+}
+
+void main() {
+  runApp(const ListDemo(type: ListDemoType.twoline));
+}
